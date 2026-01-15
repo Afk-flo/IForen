8ccd3f4e53a6209e3dccb29e38da2be71fa6d19c
Flo <62881205+Afk-flo@users.noreply.github.com>
2026-01-15 08:13:26 +0100

    Delete deviceInfoManager.py

--- func/deviceInfoManager.py
+++
@@ -1,9 +0,0 @@
-"""
-    Device info Functions
-    @by Afk-Flo 
-    @Creation data 15/09/24
-
-    Before processing to the backup, it's always interesting to collect some informations about the mobile.
-    This file will also check for outdated version (IOS 11-) and other usefull informations
-    
-"""
